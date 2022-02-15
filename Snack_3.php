ecdfb2abb87912bc14fa16164db84c1726039483
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-15 17:03:05 +0100

    Refactoring:  Snack_3

--- Snack_3.php
+++ Snack_3.php
@@ -1,14 +1,13 @@
 <?php
     $posts = [
-
         '10/01/2019' => ['Testo post 1','Testo post 2'],
         '10/02/2019' => ['Testo post 3'],
         '15/05/2019' => ['Testo post 4','Testo post 5','Testo post 6']
     ];
 
     foreach ($posts as $key => $value) {
-        for ($i=0; $i < count($value); $i++) { 
-            echo $key."<br>";
-            echo $value[$i]."<br>";
-        }
-    }
+        echo "<h4>".$key."</h4>";
+       for ($i=0; $i < count($value); $i++) {
+           echo "<div>".$value[$i]."</div>";
+       }
+   }
